ca0da966a1d01c14445b7c26285a60291e9dafa1
theoriginalbit <1377564+theoriginalbit@users.noreply.github.com>
2024-06-09 19:44:55 +1000

    Add basic first validation

--- Package.swift
+++ Package.swift
@@ -1,14 +1,9 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "EnvVar",
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "EnvVar"),
+        .executableTarget(name: "EnvVar"),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,18 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  main.swift
+//  EnvVar
+//
+//  Created by Joshua Asbury on 9/6/2024.
+//
 
-print("Hello, world!")
+import Foundation
+
+func print(envVar key: String) {
+    let value = ProcessInfo.processInfo.environment[key]
+    print(key, value ?? "not set")
+}
+
+print(envVar: "DAY_OF_WEEK")
+print(envVar: "CI")
+print(envVar: "CONFIGURATION")
+print("Hello, World!")
